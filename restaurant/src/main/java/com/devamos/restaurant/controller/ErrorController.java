bdbb84c55330ee9aaba88febe8e88ec18fd0fb99
Amos Uzuagie <amosuzuagie@gmail.com>
2025-09-05 03:02:54 +0100

    feat(photo): add retrieval endpoint with global error handling and standardized error responses

---
+++ restaurant/src/main/java/com/devamos/restaurant/controller/ErrorController.java
@@ -0,0 +1,54 @@
+package com.devamos.restaurant.controller;
+
+import com.devamos.restaurant.domain.dtos.ErrorDto;
+import com.devamos.restaurant.exceptions.BaseException;
+import com.devamos.restaurant.exceptions.StorageException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@ControllerAdvice
+public class ErrorController {
+    @ExceptionHandler(StorageException.class)
+    public ResponseEntity<ErrorDto> handleStorageException(StorageException ex) {
+        log.error("Caught StorageException", ex);
+
+        ErrorDto errorDto = ErrorDto.builder()
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .message("Unable to save or retrieve resource at this time.")
+                .build();
+
+        return new ResponseEntity<>(errorDto, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    // Handling base application exception
+    @ExceptionHandler(BaseException.class)
+    public ResponseEntity<ErrorDto> handleBaseException(BaseException ex) {
+        log.error("Caught BaseException", ex);
+
+        ErrorDto errorDto = ErrorDto.builder()
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .message("An unexpected error occurred.")
+                .build();
+
+        return new ResponseEntity<>(errorDto, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    //Catch all for unexpected exception
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDto> handleException(Exception ex) {
+        log.error("Caught unexpected exception.", ex);
+
+        ErrorDto errorDto = ErrorDto.builder()
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .message("An unexpected error occurred.")
+                .build();
+
+        return new ResponseEntity<>(errorDto, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
